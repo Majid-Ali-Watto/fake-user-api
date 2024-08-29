f02dcbaa1f4ba010af669645ab90bd0736b38ce0
Majid Ali <majid@teresol.com>
2024-08-29 23:11:24 +0500

    build command added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// In src/index.js
 import express from "express";
 import pkg from "body-parser";
 const { json } = pkg;
+import { config } from "dotenv";
+config();
 import v1UserRouter from "./v1/routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// *** ADD ***
 app.use(json());
 app.use("/api/v1/users", v1UserRouter);
 
