155d2f09ea0942a4fda9610d554363357d415fdb
Majid Ali Watto <majidaliqau@gmail.com>
2025-08-10 22:07:41 +0500

    vercel fix

---
+++ src/index.js
@@ -0,0 +1,21 @@
+"use strict";
+import express from "express";
+import pkg from "body-parser";
+const { json } = pkg;
+import { config } from "dotenv";
+config();
+import v1UserRouter from "./v1/routes/userRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(json());
+app.use("/api/v1/users", v1UserRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
